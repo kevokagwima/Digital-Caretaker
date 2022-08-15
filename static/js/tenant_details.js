edfb3653dc235d67c9521177c94b142653bdcaf7
kevin <you@example.com>
2022-08-15 16:24:49 +0300

    Updated Files

--- static/js/tenant_details.js
+++ static/js/tenant_details.js
@@ -33,7 +33,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
+    this.classList.toggle("actives");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
