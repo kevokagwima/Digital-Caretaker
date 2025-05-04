5cc1c9a73513035d9606661e34d4d77f7f5898de
kevin <you@example.com>
2025-05-04 22:39:52 +0300

    updated files

---
+++ Landlords/aws_credentials.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class awsCredentials:
+  aws_access_key = "AKIAW3MEB6Z73SXKQOUO"
+  aws_secret_key = "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"
+  bucket_name = "property-management-system-1"
+  region = "eu-north-1"
