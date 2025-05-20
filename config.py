baab9869b124fd71f1dc331c21261e3f2aef8025
kevokagwima <kevokagwima@gmail.com>
2025-05-20 19:54:47 +0300

    Updated files

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-  SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root:Hunter9039@localhost/pms"
+  # SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root:Hunter9039@localhost/pms"
+  SQLALCHEMY_DATABASE_URI = ("Postgres://", "postgresql://", 1)
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SESSION_PERMANENT = False
   SESSION_TYPE = "filesystem"
